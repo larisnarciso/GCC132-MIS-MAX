5d44b8b749d1c96561a7b84b8550937c9b72c91b
Larissa Narciso <larisnarciso@gmail.com>
2023-02-28 23:28:47 -0300

    alteração import de componente

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,13 @@
-import FindMovieService from './domain/services/findMoviesService';
-import RemoteOpenMovieRepository from './data/remote/remoteOpenMovieRepository';
-import HomePage from './view/home/HomePage';
+import FindMovieService from "./domain/services/findMoviesService";
+import RemoteOpenMovieRepository from "./data/remote/remoteOpenMovieRepository";
+import HomePage from "../app/view/Home/HomePage";
 
 const App: React.FC = () => {
   const createFindMovieService = () => {
-    return new FindMovieService(new RemoteOpenMovieRepository())
+    return new FindMovieService(new RemoteOpenMovieRepository());
   };
 
-  return (
-    <HomePage service={createFindMovieService()} />
-  );
-}
+  return <HomePage service={createFindMovieService()} />;
+};
 
 export default App;
--- src/app/view/Home/HomePage.tsx
+++ src/app/view/Home/HomePage.tsx
@@ -1,24 +1,24 @@
 import React from "react";
 import FindMovieService from "../../domain/services/findMoviesService";
-import BodyPage from "./body/BodyPage";
-import FooterPage from "./footer/FooterPage";
-import HeaderPage from "./header/HeaderPage";
-import './HomePage.css';
+import BodyPage from "../Home/Body/BodyPage";
+import FooterPage from "../Home/Footer/FooterPage";
+import HeaderPage from "../Home/Header/HeaderPage";
+import "./HomePage.css";
 
 type Props = {
-    service: FindMovieService;
-}
+  service: FindMovieService;
+};
 
 const HomePage: React.FC<Props> = ({ service }) => {
-    return (
-        <div className="backgrond-home">
-            <HeaderPage />
-            <div className="body-container">
-                <BodyPage service={service} />
-            </div>
-            <FooterPage />
-        </div>
-    );
-}
+  return (
+    <div className="backgrond-home">
+      <HeaderPage />
+      <div className="body-container">
+        <BodyPage service={service} />
+      </div>
+      <FooterPage />
+    </div>
+  );
+};
 
 export default HomePage;
